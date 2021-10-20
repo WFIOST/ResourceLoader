5e3639ebb079c644b9cc7b1a0748837f63a80f30
Frityet <ambhogal01@gmail.com>
2021-10-20 11:42:24 -0700

    Init

---
+++ ResourceLoader/ResourceLoader.Core/Plugin.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+
+namespace ResourceLoader.Core
+{
+    [BepInPlugin(PluginDescriptor.GUID, PluginDescriptor.NAME, PluginDescriptor.VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public Plugin()
+        {
+            Logger.LogInfo("Started ResourceLoader!");
+        }
+    }
+}
---
+++ ResourceLoader/ResourceLoader.Core/PluginDescriptor.cs
@@ -0,0 +1,9 @@
+namespace ResourceLoader.Core
+{
+    public static class PluginDescriptor
+    {
+        public const string GUID = "com.wfiost.resourceloader";
+        public const string NAME = "Resource Loader";
+        public const string VERSION = "1.0.0";
+    }
+}
